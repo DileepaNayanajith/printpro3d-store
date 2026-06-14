18dd3814b9e0e111f3c77f859b8ec772b2428379
Dileepa Nayanajith <dileepanayanajith380@gmail.com>
2026-06-14 14:00:35 +0530

    Setup Vue Router and Home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="min-h-screen bg-black text-white flex items-center justify-center">
-    <h1 class="text-5xl font-bold">
-      PRINTPRO3D STORE
-    </h1>
-  </div>
+  <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+</script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>APP WORKING</h1>
+</template>
